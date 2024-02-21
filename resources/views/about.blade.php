c34fbf1265e39aa62c3214b6135ba1f106aa478e
trendyjosh <trendyjosh@gmail.com>
2024-02-21 20:25:05 +0000

    Add content to about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,7 +2,7 @@
     <header>
         <h1>About</h1>
     </header>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi distinctio dolorem eligendi error expedita fugiat hic minima, mollitia, odio odit officiis quod suscipit, tempora? Aliquid culpa exercitationem perspiciatis repellendus ut.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, commodi, voluptate. Ad alias cumque debitis dolorum et facilis omnis repellendus sapiente sed veritatis. Doloremque, maxime non praesentium quos unde voluptas!</p>
+    <p>I'm a passionate software engineer from the south east of England. I have 8 years experience developing applications in PHP, I am currently focussing on Laravel & VueJS. I work for Webscript Designs, a small agency specialising in building bespoke web applications.</p>
+    <p>In my spare time I like to go camping, hiking, play football and drink plenty of beer.</p>
     <p>Design inspired by <a target="_blank" href="https://sebastiandedeyne.com/">Sebastian De Deyne</a> and <a target="_blank" href="https://carlbarenbrug.com/">Carl Barenbrug</a>.</p>
 </x-layout>
