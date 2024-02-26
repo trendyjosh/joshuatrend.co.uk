a90f43c91cdd892e6a80769f0c522a67620ee7f0
trendyjosh <trendyjosh@gmail.com>
2024-02-26 16:06:03 +0000

    Add meta description

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+    <meta name="description" content="Passionate software engineer from the south east of England. I have 8 years experience developing applications in PHP, I am currently focussing on Laravel & VueJS.">
     <title>{{ $title ? "$title |" : '' }} Joshua Trend</title>
     <link rel="icon" type="image/png" href="{{ asset('/media/jt.png') }}">
     @vite('resources/css/app.css')
