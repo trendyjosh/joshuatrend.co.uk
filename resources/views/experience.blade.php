7200cf1d23830a9a42092d1b2afbf507e90606cb
trendyjosh <trendyjosh@gmail.com>
2024-03-15 01:41:21 +0000

    Style changes on experience page

--- resources/views/experience.blade.php
+++ resources/views/experience.blade.php
@@ -8,55 +8,66 @@
 
     <h2>Webscript Designs</h2>
     <span class="project-tags">August 2021 - present</span>
-    <h3>Senior PHP Deveoper</h3>
-    <span class="project-tags">October 2022 - present</span>
-    <ul>
-        <li>Leading development on full stack Laravel 10/Vue 3 web applications</li>
-        <li>Implementing CI/CD with Docker (Sail) and Laravel Forge</li>
-        <li>Linux server management and configuration</li>
-        <li>Mentoring junior developers and establishing coding standards</li>
-    </ul>
-    <h3>PHP Deveoper</h3>
-    <span class="project-tags">August 2021 - October 2022</span>
-    <ul>
-        <li>Development and maintenance of full-stack PHP applications</li>
-        <li>Maintaining a legacy Perl and Javascript-based course booking system</li>
-        <li>Implementing version control and setting up staging environments</li>
-        <li>Working closely with UI/UX designers to meet the needs of clients</li>
-    </ul>
+    <div class="experience-entry">
+        <h3>Senior PHP Deveoper</h3>
+        <span class="project-tags">October 2022 - present</span>
+        <ul>
+            <li>Leading development on full stack Laravel 10/Vue 3 web applications</li>
+            <li>Implementing CI/CD with Docker (Sail) and Laravel Forge</li>
+            <li>Linux server management and configuration</li>
+            <li>Mentoring junior developers and establishing coding standards</li>
+        </ul>
+    </div>
+    <div class="experience-entry">
+        <h3>PHP Deveoper</h3>
+        <span class="project-tags">August 2021 - October 2022</span>
+        <ul>
+            <li>Development and maintenance of full-stack PHP applications</li>
+            <li>Maintaining a legacy Perl and Javascript-based course booking system</li>
+            <li>Implementing version control and setting up staging environments</li>
+            <li>Working closely with UI/UX designers to meet the needs of clients</li>
+        </ul>
     </div>
 
     <hr>
 
     <h2>Self Employed</h2>
     <span class="project-tags">June 2017 - present</span>
-    <h3>PHP Deveoper</h3>
-    <span class="project-tags">June 2017 - present</span>
-    <ul>
-        <li>Leading development on full stack Laravel 10/Vue 3 web applications</li>
-        <li>Implementing CI/CD with Docker (Sail) and Laravel Forge</li>
-        <li>Linux server management and configuration</li>
-        <li>Mentoring junior developers and establishing coding standards</li>
-    </ul>
+    <div class="experience-entry">
+        <h3>PHP Deveoper</h3>
+        <span class="project-tags">June 2017 - present</span>
+        <ul>
+            <li>Leading development on full stack Laravel 10/Vue 3 web applications</li>
+            <li>Implementing CI/CD with Docker (Sail) and Laravel Forge</li>
+            <li>Linux server management and configuration</li>
+            <li>Mentoring junior developers and establishing coding standards</li>
+        </ul>
+    </div>
 
     <hr>
 
     <h2>NHS Trust</h2>
     <span class="project-tags">February 2017 - August 2021</span>
-    <h3>Clinical Systems Analyst</h3>
-    <span class="project-tags">October 2020 - August 2021</span>
-    <ul>
-        <li>Liaising with different departments to determine requirements</li>
-        <li>QA testing new systems</li>
-        <li>Running database reports</li>
-    </ul>
-    <h3>Clinical Systems Support</h3>
-    <span class="project-tags">July 2019 - October 2020</span>
-    <ul>
-        <li>1st and 2nd line support for clinical systems</li>
-        <li>Delivering systems training to new staff</li>
-        <li>Support during new system rollouts</li>
-    </ul>
-    <h3>Health Records Clerk</h3>
-    <span class="project-tags">February 2017 - July 2019</span>
+    <div class="experience-entry">
+        <h3>Clinical Systems Analyst</h3>
+        <span class="project-tags">October 2020 - August 2021</span>
+        <ul>
+            <li>Liaising with different departments to determine requirements</li>
+            <li>QA testing new systems</li>
+            <li>Running database reports</li>
+        </ul>
+    </div>
+    <div class="experience-entry">
+        <h3>Clinical Systems Support</h3>
+        <span class="project-tags">July 2019 - October 2020</span>
+        <ul>
+            <li>1st and 2nd line support for clinical systems</li>
+            <li>Delivering systems training to new staff</li>
+            <li>Support during new system rollouts</li>
+        </ul>
+    </div>
+    <div class="experience-entry">
+        <h3>Health Records Clerk</h3>
+        <span class="project-tags">February 2017 - July 2019</span>
+    </div>
 </x-layout>
