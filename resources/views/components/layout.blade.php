84140cf2d9b60c54263ba3ce0f92dba009a0541a
trendyjosh <trendyjosh@gmail.com>
2024-03-15 17:38:05 +0000

    Add html signature

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,25 +1,47 @@
-
 <!DOCTYPE html>
 <html lang="en">
+<!-- 
+                 vvvvvv
+                / _  _ \
+              (| (.)(.) |)
+ __________.000o___()___o000._________
+|      ___        _                   |
+|     |_  |___ __| |_ _  _ __ _       |
+|     __| / _ \_-< ' \ || / _` |      |
+|     \__,\___/__/_||_\_,_\__,_|      |
+|      |_   _| _ ___ _ _  __| |       |
+|        | || '_/ -_) ' \/ _` |       |
+|        |_||_| \___|_||_\__,_|       |
+|   .ooo0                     0ooo.   |
+|___(   )_____________________(   )___|
+     \ ( /                   \ ) /
+      \_)                     (_/
+
+Website built by Joshua Trend (contact@joshuatrend.co.uk), design inspired by Sebastian De Deyne (https://sebastiandedeyne.com/) and Carl Barenbrug (https://carlbarenbrug.com/).
+View the source code here: https://github.com/trendyjosh/joshuatrend.co.uk
+ -->
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
     <meta name="description" content="Passionate software engineer from the south east of England. I have 8 years experience developing applications in PHP, I am currently focussing on Laravel & VueJS.">
-    <title>{{ $title ? "$title |" : '' }} Joshua Trend</title>
+    <title>{{ $title ? "$title | " : '' }}Joshua Trend</title>
     <link rel="icon" type="image/png" href="{{ asset('/media/jt.png') }}">
     @vite('resources/css/app.css')
 </head>
+
 <body>
-<div class="grid">
-    <x-nav :title="$title" />
-    <article>
-        {{ $slot }}
-    </article>
-    <footer class="footer">
-        <p>
-            <a href="{{ route('index') }}">Joshua Trend</a> | 2024
-        </p>
-    </footer>
-</div>
+    <div class="grid">
+        <x-nav :title="$title" />
+        <article>
+            {{ $slot }}
+        </article>
+        <footer class="footer">
+            <p>
+                <a href="{{ route('index') }}">Joshua Trend</a> | 2024
+            </p>
+        </footer>
+    </div>
 </body>
+
 </html>
