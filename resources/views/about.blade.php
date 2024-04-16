fd8e38e265f14205a4a2209271e14ba882b28c08
trendyjosh <trendyjosh@gmail.com>
2024-04-16 12:04:30 +0100

    Add image of me to the about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,5 +4,8 @@
     </header>
     <p>I'm a passionate software engineer from the south east of England. I have 8 years experience developing applications in PHP, I am currently focussing on Laravel & VueJS. I work for <a target="_blank" href="https://webscript.co.uk/">Webscript Designs</a>, a small agency specialising in building bespoke web applications.</p>
     <p>In my spare time I enjoy camping, hiking, playing football and drinking beer.</p>
+    <figure>
+        <img src="{{ asset('/media/joshua-trend.jpg') }}" alt="Joshua Trend">
+    </figure>
     <p>Design inspired by <a target="_blank" href="https://sebastiandedeyne.com/">Sebastian De Deyne</a> and <a target="_blank" href="https://carlbarenbrug.com/">Carl Barenbrug</a>.</p>
 </x-layout>
