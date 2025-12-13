4c9b9fe010182e8849051a028236fe405c0a8d34
trendyjosh <trendyjosh@gmail.com>
2025-12-13 00:46:41 +0000

    feat: add webp homepage image

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,7 +5,11 @@
     <p>I'm a passionate software engineer from the south east of England. I have 8 years experience developing applications in PHP, I am currently focussing on Laravel & VueJS. I work for <a target="_blank" href="https://webscript.co.uk/">Webscript Designs</a>, a small agency specialising in building bespoke web applications.</p>
     <p>In my spare time I enjoy camping, hiking, playing football and drinking beer.</p>
     <figure>
-        <img src="{{ asset('/media/joshua-trend.jpg') }}" alt="Joshua Trend">
+        <picture>
+            <source srcset="{{ asset('/media/joshua-trend.webp') }}" type="image/webp">
+            <img src="{{ asset('/media/joshua-trend.jpg') }}" alt="Joshua Trend" 
+                width="607" height="361" fetchpriority="high">
+        </picture>
     </figure>
     <p>Website design inspired by <a target="_blank" href="https://sebastiandedeyne.com/">Sebastian De Deyne</a> and <a target="_blank" href="https://carlbarenbrug.com/">Carl Barenbrug</a>.</p>
 </x-layout>
