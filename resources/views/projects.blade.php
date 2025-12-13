ec6922d57b63e25f320fcb6c52bd59f63f882cf8
trendyjosh <trendyjosh@gmail.com>
2025-12-13 04:39:07 +0000

    feat: project image enhancements

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -11,7 +11,11 @@
     <p>A game where the objective is to create the longest chain of related movies and their actors that can be guessed. Answer verification provided by the <a href="https://www.themoviedb.org/">TMDB</a> API.</p>
     <p>Deployed using Laravel Forge.</p>
     <figure>
-        <img src="{{ asset('/media/movie-clash.jpg') }}" alt="Movie Clash game page">
+        <picture>
+            <source srcset="{{ asset('/media/movie-clash.webp') }}" type="image/webp">
+            <img src="{{ asset('/media/movie-clash.jpg') }}" alt="Movie Clash game page" 
+                width="607" height="334" fetchpriority="high">
+        </picture>
         <figcaption>A solo game being played</figcaption>
     </figure>
 
@@ -22,7 +26,11 @@
     <p>A very simple CMS for the purpose of generating invoices for customers.</p>
     <p>Deployed using Laravel Forge.</p>
     <figure>
-        <img src="{{ asset('/media/invoice-system.jpg') }}" alt="Invoice System Invoices Page">
+        <picture>
+            <source srcset="{{ asset('/media/invoice-system.webp') }}" type="image/webp">
+            <img src="{{ asset('/media/invoice-system.jpg') }}" alt="Invoice System Invoices Page" 
+                width="607" height="312" fetchpriority="high">
+        </picture>
         <figcaption>The invoice management page</figcaption>
     </figure>
 
@@ -32,7 +40,11 @@
     <span class="project-tags">#Laravel, #Blade, #TailwindCSS, #VueJS</span>
     <p>A Garden Design portfolio website and basic CMS for a Garden Designer in Kent to showcase completed projects. Developed with vanilla CSS and JavaScript for the front-end site, then Tailwind and VueJS for the admin panel. The site also makes use of the Instagram API to display the most recent posts.</p>
     <figure>
-        <img src="{{ asset('/media/cameron-trend-design.jpg') }}" alt="Cameron Trend Design Landing Page">
+        <picture>
+            <source srcset="{{ asset('/media/cameron-trend-design.webp') }}" type="image/webp">
+            <img src="{{ asset('/media/cameron-trend-design.jpg') }}" alt="Cameron Trend Design Landing Page" 
+                width="607" height="316" fetchpriority="high">
+        </picture>
         <figcaption>The website landing page</figcaption>
     </figure>
     <p>Future development will include traffic metrics provided by the Cloudflare Graph API.</p>
