38e85e48d12bb6ac1eb5b463512995b97af0ec44
trendyjosh <trendyjosh@gmail.com>
2026-04-10 12:55:25 +0100

    feat: update work experience

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,7 +2,7 @@
     <header>
         <h1>About</h1>
     </header>
-    <p>I'm a passionate software engineer from the south east of England. I have 8+ years experience developing applications in PHP, I am currently focussing on Laravel & VueJS. I work for <a target="_blank" href="https://www.vital-parts.co.uk/">Vital Parts Ltd</a>, a plastic and rubber components manufacturer and distributor.</p>
+    <p>I'm a passionate software engineer from the south east of England. I have 8+ years experience developing applications in PHP, I am currently focussing on Laravel & VueJS. I work for <a target="_blank" href="https://www.vital-parts.co.uk/">Vital Parts Ltd</a>, a plastic, metal and rubber components manufacturer and distributor.</p>
     <p>In my spare time I enjoy camping, hiking, playing football and drinking beer.</p>
     <figure>
         <picture>
